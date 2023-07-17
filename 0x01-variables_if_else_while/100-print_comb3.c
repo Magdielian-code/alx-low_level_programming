d0ebbfbd086c453b7a4ee33eaa81ac1703c28f2b
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-17 13:33:58 -0700

    100-

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of code
+ * main - Entry poin
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int digit1, digit2;
 
+	/* Loop through possible combinations of two digits */
 	for (digit1 = 0; digit1 < 9; digit1++)
 	{
-	for (digit2 = digit1 + 1; digit2 < 10; digit2++)
-	{
-	putchar((digit1 % 10) + '0');
-	putchar((digit2 % 10) + '0');
-		if (digit1 == 8 && digit2 == 9)
-			continue;
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			/* Add comma and space after all combination */
+			if (digit1 == 8 && digit2 == 9)
+				continue;
 			putchar(',');
 			putchar(' ');
 		}
