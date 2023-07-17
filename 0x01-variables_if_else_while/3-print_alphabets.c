38e723a61130f9df1d03274b7cb8d37cd74093cb
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-17 13:44:11 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,24 +1,26 @@
 #include <stdio.h>
 
 /**
- * main - Entry point of the program
- *
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	char letter;
-	
+
+	/* Prints lowercase alphabet */
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-	putchar(letter);
+		putchar(letter);
 	}
-	
+
+	/* Prints uppercase alphabet */
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
-	putchar(letter);
+		putchar(letter);
 	}
+
 	putchar('\n');
+
 	return (0);
-}
+}}
