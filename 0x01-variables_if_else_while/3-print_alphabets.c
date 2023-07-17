00fc7b79f7cc962e5f83c602eddcc7186df496b6
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-17 12:01:51 -0700

    3

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -12,14 +12,13 @@ int main(void)
 	
 	for (letter = 'a'; letter <= 'z'; letter++)
 	{
-		putchar(letter);
+	putchar(letter);
 	}
 	
 	for (letter = 'A'; letter <= 'Z'; letter++)
 	{
-		putchar(letter);
+	putchar(letter);
 	}
-
 	putchar('\n');
 	return (0);
 }
