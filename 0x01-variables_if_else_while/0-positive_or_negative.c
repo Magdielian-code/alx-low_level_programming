b29bc09cc6d0558b7af99a8e471a5d2a2ac5606c
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-17 10:49:50 -0700

    0-

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -15,13 +15,13 @@ int main(void)
 	
 	if (n > 0)
 	{
-    		printf("n is positive\n", n);
+    		printf("%i n is positive\n", n);
 	} else if (n == 0)
 	{
-		printf("n, is zero\n", n);
+		printf("%i n is zero\n", n);
 	} else if (n < 0)
 	{
-    		printf("%n, is negative\n", n);
+    		printf("%i n, is negative\n", n);
 	}
 
 	return (0);
