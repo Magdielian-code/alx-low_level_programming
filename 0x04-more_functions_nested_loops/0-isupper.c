8d8df0fab5b0489f9dadf888b7e82822662d80f8
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-20 06:27:10 -0700

    0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * this program cheks for uppercase letters
+ * _isupper - Checks if a character is uppercase
  * @c: The character to be checked
  *
  * Return: 1 if c is uppercase, 0 otherwise
  */
 int _isupper(int c)
 {
-	/* Check if the character is an uppercase letter */
+	/* Check if the character is within the range of uppercase letters */
 	if ((c >= 'A') && (c <= 'Z'))
 		return (1);
 	else
