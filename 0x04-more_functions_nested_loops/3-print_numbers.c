c00a4dfe2dff60fbe171c670918f235ea3b941a0
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-07-20 16:39:15 -0700

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *Prints 0123456789 followed by a new line
+ *
+ * Return: void
+ */
+void print_numbers(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
