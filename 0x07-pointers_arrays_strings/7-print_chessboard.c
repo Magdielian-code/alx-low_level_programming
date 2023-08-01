c7bb58dfbb0779695de3bf560be7ba8f93f9c560
Magdielian-code <oziomaagaecheta81@gmail.com>
2023-08-01 09:40:56 -0700

    7

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: input pointer to a 2D array representing the chessboard.
+ * Return: no return.
+ */
+void print_chessboard(char (*a)[8])
+{
+	unsigned int i, m = 0;
+
+	for (i = 0; i < 64; i++)
+	{
+		if (i % 8 == 0 && i != 0)
+		{
+			m = i;
+			_putchar('\n');
+		}
+		_putchar(a[i / 8][i - m]);
+	}
+	_putchar('\n');
+}
