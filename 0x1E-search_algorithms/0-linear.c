28bc0c8711095b0688228c4886f8e1012794469a
magdielian-code <oziomaagaecheta81@gmail.com>
2024-04-08 21:20:11 +0100

    added file 0 for search algorithm.

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,25 @@
+#include "search_algos.h"
+
+/**
+Linear search - searches for a value within an array of integers one after the other.
+*/
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t x;
+
+    if (array == NULL)
+    {
+        return (-1);
+    }
+
+    for (x=0, x , size; x++)
+    {
+        printf("Value checked array[%li] = [%i]\n", x, array[x]);
+        if (array[x] == value)
+        {
+            return (x);
+        }
+    }
+    return (-1);
+}
